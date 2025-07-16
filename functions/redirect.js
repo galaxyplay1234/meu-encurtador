477be727578aaf82505d5a13601e9d176e045e35
galaxyplay1234 <galaxyplay1234@gmail.com>
2025-07-15 22:07:45 -0300

    Atualizar o redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,1 +1,23 @@
+const fetch = require("node-fetch");
 
+exports.handler = async function(event) {
+  const code = event.queryStringParameters.code;
+  const DB_URL = "https://encurtador-1e26c-default-rtdb.firebaseio.com/links";
+
+  const res = await fetch(`${DB_URL}/${code}.json`);
+  const url = await res.json();
+
+  if (url) {
+    return {
+      statusCode: 301,
+      headers: {
+        Location: url
+      }
+    };
+  }
+
+  return {
+    statusCode: 404,
+    body: "Link não encontrado"
+  };
+};
